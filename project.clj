0a408073957ec798f7d7fdec1c6736b42762a3e2
sparkofreason <dave.d.dixon@gmail.com>
2018-02-27 14:45:33 -0800

    Move common namespace from todo.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/spec.alpha "0.1.143"]
+                 [org.clojure/test.check "0.9.0"]
                  [com.cerner/clara-rules "0.18.0-SNAPSHOT"]]
   :source-paths ["src" "test"])
