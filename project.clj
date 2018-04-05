8fce34af14a10ab3e57ee78af89a5a4201b2f15b
sparkofreason <dave.d.dixon@gmail.com>
2018-04-05 09:52:54 -0700

    clara-rules 0.18.0

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
                  [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/test.check "0.9.0"]
-                 [com.cerner/clara-rules "0.18.0-SNAPSHOT"]]
+                 [com.cerner/clara-rules "0.18.0"]]
   :source-paths ["src" "test"])
